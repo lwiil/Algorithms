490d5ff4a9a53980b81ae6d7209875df0a94bcce
lwiil <lkalabokl@gmail.com>
2018-09-13 18:10:36 +0300

    add lesson 7 task 1

--- L7T1.py
+++ L7T1.py
@@ -1,1 +1,18 @@
-  Есть надежда, что здесь появится дз ближе к вечеру.
+# 1. Отсортировать по убыванию методом «пузырька» одномерный целочисленный массив, заданный случайными числами на
+# промежутке [-100; 100). Вывести на экран исходный и отсортированный массивы.
+
+import random
+
+size = 20
+arr = [random.randint(-100, 100) for _ in range(size)]
+sorted_arr = []
+print(arr)
+
+counter = len(arr)
+
+while counter:
+    for i in range(len(arr) - 1):
+        if arr[i] < arr[i + 1]:
+            arr[i], arr[i + 1] = arr[i + 1], arr[i]
+    counter -= 1
+print(arr)
